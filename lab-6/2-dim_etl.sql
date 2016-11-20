255221332ff001ddc3432c8e479f758c7397697a
Zontzip <zontzor@gmail.com>
2016-11-20 23:18:46 +0000

    Start second load

--- lab-6/2-dim_etl.sql
+++ lab-6/2-dim_etl.sql
@@ -1,4 +1,12 @@
-/* ETL team dimension */ 
+drop table stage_teams cascade constraints purge;
+drop table stage_players cascade constraints purge;
+drop table stage_tournaments cascade constraints purge;
+drop table stage_dates cascade constraints purge;
+drop table stage_facts cascade constraints purge;
+
+/***********************
+ * ETL team dimension
+ **********************/
 drop table stage_teams cascade constraints purge;
 
 create table stage_teams (
@@ -49,7 +57,9 @@ insert into dim_teams (team_sk, team_name)
 and source_db = 2; 
 -- End team ETL
 
-/* ETL players dimension */
+/*************************
+ * ETL players dimension
+ ************************/
 drop table stage_players cascade constraints purge;
 
 create table stage_players (
@@ -106,7 +116,9 @@ insert into dim_players (player_sk, player_name)
   and source_db = 2; 
 -- End player ETL
 
-/* ETL tournament dimension */
+/****************************
+ * ETL tournament dimension
+ ***************************/
 drop table stage_tournaments cascade constraints purge;
 
 create table stage_tournaments (
@@ -149,7 +161,9 @@ insert into dim_tournaments (tournament_sk, tournament_desc, total_prize)
   from stage_tournaments;
 -- End tournament ETL  
 
-/* ETL date sk */
+/************************
+ * ETL date dimension
+ ***********************/
 drop table stage_dates cascade constraints purge;
 
 create table stage_dates (
@@ -160,7 +174,8 @@ create table stage_dates (
   d_year integer,
   d_week integer,
   d_quarter integer,
-  d_dayofweek integer
+  d_dayofweek integer,
+  tour_date date
 );
 
 drop sequence date_stage_seq;
@@ -170,8 +185,6 @@ start with 1
 increment by 1 
 nomaxvalue; 
 
-drop trigger stage_date_trigger;
-
 create trigger stage_date_trigger
 before insert on stage_dates
 for each row
@@ -180,15 +193,28 @@ select date_stage_seq.nextval into :new.date_sk from dual;
 end;
 
 insert into stage_dates (source_db, d_day, d_month, d_year, d_week, d_quarter, 
-d_dayofweek)
+d_dayofweek, tour_date)
+select  1,
+        cast(to_char(t_date,'DD') as integer),
+        cast(to_char(t_date,'MM') as integer),
+        cast(to_char(t_date,'YYYY') as integer),
+        cast(to_char(t_date,'WW') as integer),
+        cast(to_char(t_date,'Q') as integer),
+        cast(to_char(t_date,'D') as integer),
+        t_date
+from tournament1;
+
+insert into stage_dates (source_db, d_day, d_month, d_year, d_week, d_quarter, 
+d_dayofweek, tour_date)
 select  2,
         cast(to_char(t_date,'DD') as integer),
         cast(to_char(t_date,'MM') as integer),
         cast(to_char(t_date,'YYYY') as integer),
         cast(to_char(t_date,'WW') as integer),
         cast(to_char(t_date,'Q') as integer),
-        cast(to_char(t_date,'D') as integer)
-from tournament1;
+        cast(to_char(t_date,'D') as integer),
+        t_date
+from tournament2;
 
 insert into dim_dates(date_sk, d_day, d_month, d_year, d_week, d_quarter, 
 d_dayofweek)
@@ -196,7 +222,9 @@ d_dayofweek)
   from stage_dates;
 -- End date ETL 
 
-/* Facts */
+/****************************
+ * ETL facts dimension
+ ***************************/
 drop table stage_facts cascade constraints purge;
 
 create table stage_facts (
@@ -209,20 +237,25 @@ create table stage_facts (
   player_id integer,
   tour_id integer,
   team_id integer,
+  tour_date date,
   source_db integer
 );
 
-insert into stage_facts (f_rank, f_prize, player_id, tour_id, team_id, source_db)
-  select r1.rank, r1.price, r1.p_id, r1.t_id, t1.team_id, 1
+insert into stage_facts (f_rank, f_prize, player_id, tour_id, team_id, 
+tour_date, source_db)
+  select r1.rank, r1.price, r1.p_id, r1.t_id, t1.team_id, tn1.t_date, 1
   from results1 r1
   join players1 p1 on p1.p_id = r1.p_id
-  join team1 t1 on t1.team_id = p1.team_id;
+  join team1 t1 on t1.team_id = p1.team_id
+  join tournament1 tn1 on tn1.t_id = r1.t_id;
 
-insert into stage_facts (f_rank, f_prize, player_id, tour_id, team_id, source_db)
-  select r2.rank, r2.price, r2.p_id, r2.t_id, t2.team_id, 2
+insert into stage_facts (f_rank, f_prize, player_id, tour_id, team_id, 
+tour_date, source_db)
+  select r2.rank, r2.price, r2.p_id, r2.t_id, t2.team_id, tn2.t_date, 2
   from results2 r2
   join players2 p2 on p2.p_id = r2.p_id
-  join team2 t2 on t2.team_id = p2.team_id;
+  join team2 t2 on t2.team_id = p2.team_id
+  join tournament2 tn2 on tn2.t_id = r2.t_id;
 
 update stage_facts
 set f_prize = (f_prize * 0.7)
@@ -255,4 +288,23 @@ set team_sk = (
 );
 
 /* Date SK */
+update stage_facts sf
+set date_sk = (
+  select date_sk
+  from stage_dates sd
+  where sd.source_db = sf.source_db 
+  and sd.tour_date = sf.tour_date
+);
+
+/***********************
+ * New data
+ **********************/
+insert into players1 (p_id, p_name, p_sname, team_id) values (7, 'alan', 'parker', 1);
+insert into players1 (p_id, p_name, p_sname, team_id) values (8, 'martha', 'bag', 2);
+insert into tournament1 (t_id, t_descriprion, total_price) values (5, 'saudi open', 500000);
+
+insert into results1 (t_id, p_id, rank, price) values (5, 7, 6, 10000);
+/*insert into results1 (t_id, p_id, rank, price) values (5, 7, 5, 20000); */
+insert into results1 (t_id, p_id, rank, price) values (2, 8, 3, 1000);
+
 
