b016068bd48526dc5a5049ee44962912fbed3a09
Zontzip <zontzor@gmail.com>
2016-11-25 21:41:17 +0000

    Add json data

---
+++ lab-7/exam_results.js
@@ -0,0 +1,62 @@
+db.results.insert({
+  "name": "Mary",
+  "surname": "Murray",
+  "nationality": "Irish",
+  "age": "45",
+  "courses": [
+    {"name": "Databases", "credits": 10},
+    {"name": "Maths", "credits": 5},
+    {"name": "Programming", "credits": 15}
+  ],
+  "results": [
+    {"name": "Databases", "mark": 56, "date": "10/10/2011"},
+    {"name": "Maths", "mark": 76, "date": "09/11/2012"},
+    {"name": "Programming", "mark": 45, "date": "02/07/2014"}
+  ]
+});
+db.results.insert({
+  "name": "Bill",
+  "surname": "Bellichick",
+  "nationality": "American",
+  "age": "32",
+  "courses": [
+    {"name": "Databases", "credits": 10},
+    {"name": "Maths", "credits": 5},
+    {"name": "Programming", "credits": 15}
+  ],
+  "results": [
+    {"name": "Databases", "mark": 55, "date": "10/10/2011"},
+    {"name": "Maths", "mark": 87, "date": "09/11/2012"},
+    {"name": "Programming", "mark": 45, "date": "10/10/2011"}
+  ]
+});
+db.results.insert({
+  "name": "Tom",
+  "surname": "Brady",
+  "nationality": "Canadian",
+  "age": "22",
+  "courses": [
+    {"name": "Databases", "credits": 10},
+    {"name": "Maths", "credits": 5}
+  ],
+  "results": [
+    {"name": "Databases", "mark": 34, "date": "09/11/2012"},
+    {"name": "Maths", "mark": 56, "date": "02/07/2014"}
+  ]
+});
+db.results.insert({
+  "name": "John",
+  "surname": "Bale",
+  "nationality": "English",
+  "age": "24",
+  "courses": [
+    {"name": "Databases", "credits": 10},
+    {"name": "Maths", "credits": 5},
+    {"name": "Programming", "credits": 15}
+  ],
+  "results": [
+    {"name": "Databases", "mark": 71, "date": "10/10/2011"},
+    {"name": "Maths", "mark": 88, "date": "10/10/2011"},
+    {"name": "Programming", "mark": 95, "date": "09/11/2012"}
+  ]
+});
