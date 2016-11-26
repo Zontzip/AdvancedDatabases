d806557b048891401b65bb2b72ea40b44cfc7440
Zontzip <zontzor@gmail.com>
2016-11-26 21:50:20 +0000

    Add q2 query

--- lab-7/queries.js
+++ lab-7/queries.js
@@ -3,9 +3,35 @@ db.C13451458_schema.find(
   {
     results: {
       $elemMatch: {
-        mark:
-        {lt$: 40}
+        mark: {
+          $lt: 40
+        }
       }
     }
   }
-)
+);
+
+// 2. Find the number of people that passed each exam
+db.C13451458_schema.aggregate(
+  [
+    {
+      $unwind : "$results"
+    },
+    {
+      $match:
+      {
+        "results.mark" : {
+          "$gte" : 40
+        }
+      }
+    },
+    {
+      $group : {
+        _id: "$results.name",
+        total : {
+          $sum: 1
+        }
+      }
+    }
+  ]
+);
